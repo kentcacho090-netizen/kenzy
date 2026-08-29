3197d645760642006b50e4cd321bf13303243496
kentcacho090-netizen <kentcacho090@gmail.com>
2026-08-29 16:00:55 +0800

    Connect Kenzy workspace navigation

--- src/index.js
+++ src/index.js
@@ -2,6 +2,8 @@ import React from 'react';
 import { createRoot } from 'react-dom/client';
 import App from './App';
 import './App.css';
+import './navigation.css';
+import './navigation';
 
 const root = createRoot(document.getElementById('root'));
 root.render(
